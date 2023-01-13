beb090da12949d2b20060d85c7fed91a0589f4ce
Jacob Biros <jakebiros@gmail.com>
2023-01-13 22:26:41 +0900

    Add backprop function

--- src/Network/network.cpp
+++ src/Network/network.cpp
@@ -1,5 +1,6 @@
 #include "network.h"
 #include "activations.h"
+#include "cross_entropy.h"
 #include <Eigen/Dense>
 #include <vector>
 #include <iostream>
@@ -8,9 +9,11 @@ using Eigen::MatrixXf;
 using Eigen::VectorXf;
 using std::vector;
 
-Network::Network(vector<int> sizes) {
+Network::Network(vector<int> sizes, float learning_rate) {
 
     m_sizes = sizes;
+
+    m_learning_rate = learning_rate;
     
     Network::initializeWeightsAndBiases();
 
@@ -37,12 +40,58 @@ MatrixXf Network::forward(MatrixXf input) {
     for (int idx = 0; idx < m_weights.size(); idx++) {
         output = m_weights[idx] * output;
         output.colwise() += m_biases[idx];
+        m_layer_outputs.push_back(output);
+
         output = sigmoid(output);
+        m_layer_activations.push_back(output);
     }
 
-    MatrixXf probabilities = softmax(output);
+    output = softmax(output);
 
-    return probabilities;
+    return output;
+};
+
+void Network::backward(MatrixXf probabilities, std::vector<int> ground_truth) {
+
+    vector<MatrixXf> weight_gradients;
+    vector<VectorXf> bias_gradients;
+
+    // Initialize Gradient Matrices
+    for (int idx = 0; idx < m_weights.size(); idx++) {
+        MatrixXf weight_gradient_layer = m_weights[idx];
+        weight_gradient_layer.setZero();
+        weight_gradients.push_back(weight_gradient_layer);
+
+        VectorXf bias_gradient_layer = m_biases[idx];
+        bias_gradient_layer.setZero();
+        bias_gradients.push_back(bias_gradient_layer);
+    }
+
+    // Collect Gradients
+    MatrixXf delta = cross_entropy_prime(probabilities, ground_truth);    
+
+    bias_gradients.back() += delta.rowwise().sum();
+    weight_gradients.back() += delta * m_layer_activations.end()[-2].transpose();
+
+    for (int idx = m_weights.size() - 2; idx > 0; idx--) {
+
+        auto layer_output = m_layer_outputs[idx];
+        auto diff_weighted_input = sigmoid_prime(layer_output);
+
+        VectorXf delta = (m_weights[idx].transpose() * delta) * diff_weighted_input;
+
+        bias_gradients.back() = delta;
+        weight_gradients.back() = delta * m_layer_activations[idx - 1].transpose();
+    }
+
+    // Apply Gradient Descent
+    for (int idx = 0; idx < weight_gradients.size(); idx++) {
+
+        m_weights[idx] = m_weights[idx] - m_learning_rate * weight_gradients[idx];
+        m_biases[idx] = m_biases[idx] - m_learning_rate * bias_gradients[idx];
+
+    }
+
 };
 
 vector<MatrixXf> Network::getLayers() {
--- src/Network/network.h
+++ src/Network/network.h
@@ -13,20 +13,23 @@ class Network {
 
     private:
         int m_numLayers;
+        float m_learning_rate;
         vector<int> m_sizes;
         vector<MatrixXf> m_weights;
         vector<VectorXf> m_biases;
+        vector<MatrixXf> m_layer_outputs;
+        vector<MatrixXf> m_layer_activations;
 
         void initializeWeightsAndBiases();
 
     public:
-        Network(vector<int>);
+        Network(vector<int>, float learning_rate);
 
         MatrixXf forward(MatrixXf input);
 
         vector<MatrixXf> getLayers();
 
-        void backward(float loss);
+        void backward(MatrixXf probabilities, std::vector<int> ground_truth);
 
 };
 
