20431a19242a85b28318d956172394b3d304f572
Jacob Biros <jakebiros@gmail.com>
2023-01-13 22:26:10 +0900

    Add derivative of cross entropy and softmax

--- src/Loss/cross_entropy.cpp
+++ src/Loss/cross_entropy.cpp
@@ -2,6 +2,8 @@
 
 #include <cmath>
 #include <vector>
+#include <eigen/Dense>
+#include <iostream>
 
 float cross_entropy(MatrixXf predictions, std::vector<int> ground_truth) {
 
@@ -17,3 +19,14 @@ float cross_entropy(MatrixXf predictions, std::vector<int> ground_truth) {
 
     return resultant.sum();
 };
+
+MatrixXf cross_entropy_prime(MatrixXf probabilities, std::vector<int> ground_truth) {
+
+    MatrixXf truth_onehot = MatrixXf::Zero(probabilities.rows(), probabilities.cols());
+
+    for (int idx = 0; idx < probabilities.cols(); ++idx) {
+        truth_onehot.col(idx)[ground_truth[idx]] = 1;
+    }
+
+    return probabilities - truth_onehot;
+}
--- src/Loss/cross_entropy.h
+++ src/Loss/cross_entropy.h
@@ -5,8 +5,11 @@
 #include <vector>
 
 using Eigen::MatrixXf;
+using Eigen::VectorXf;
 
 float cross_entropy(MatrixXf predictions, std::vector<int> ground_truth);
+
+MatrixXf cross_entropy_prime(MatrixXf probabilities, std::vector<int> ground_truth);
 
 #endif // CROSS_ENTROPY_H
 
