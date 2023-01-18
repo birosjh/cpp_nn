efb27dce35bafe7b0c5ea4d2a2ed7f60986ba491
Jacob Biros <jakebiros@gmail.com>
2023-01-18 20:48:37 +0900

    Clean up main now that trainer has been created

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,26 @@
-#include <iostream>
-#include <Eigen/Dense>
-#include <vector>
 #include "Network/network.h"
 #include "DataLoaders/fashion_mnist_loader.h"
-#include "Utils/utils.h"
-#include "Loss/cross_entropy.h"
-#include "Metrics/accuracy.h"
+#include "Trainer/trainer.h"
 
-using Eigen::MatrixXf;
+#include <iostream>
+#include <vector>
  
 int main()
 {
+  int epochs = 5;
 
   FashionMNISTLoader dataloader = FashionMNISTLoader(16);
   
   // Define Network
   std::vector<int> sizes{ 784, 42, 10 };
-  Network nn(sizes);
+  Network nn(sizes, 0.2);
 
   // Print Network Layout
   std::cout << nn << std::endl;
+  
+  Trainer trainer(nn, dataloader, epochs);
 
-  std::cout << "Num batches: " << dataloader.num_training_batches() << std::endl;
+  trainer.fit();
 
-  // Run a Batch
-  auto batch = dataloader.nextBatch();
-
-  MatrixXf output = nn.forward(batch.images);
-
-  // std::cout << predictions << std::endl;
-
-  float loss = cross_entropy(output, batch.labels);
-
-  std::cout << "Loss: " << loss << std::endl;
-
-  // Get max of each
-  std::vector<int> predictions = argmax(output);
-
-  for (auto value : predictions) { 
-    std::cout << value << std::endl;
-  }
-
-  // Calculate Accuracy
-  auto acc = accuracy(predictions, batch.labels);
-
-  std::cout << "Accuracy: " << acc << std::endl;
+  // trainer.test();
 }
