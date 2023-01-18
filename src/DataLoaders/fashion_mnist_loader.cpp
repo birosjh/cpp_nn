437d124ae9caa5de55b157934a064a3318f4198a
Jacob Biros <jakebiros@gmail.com>
2023-01-18 20:46:30 +0900

    Revamp dataloader to reload after each loop

--- src/DataLoaders/fashion_mnist_loader.cpp
+++ src/DataLoaders/fashion_mnist_loader.cpp
@@ -8,86 +8,118 @@
 using Eigen::MatrixXf;
 using Eigen::VectorXf;
 
-FashionMNISTLoader::FashionMNISTLoader(int batch_size, bool shuffle_train) {
-    m_batch_size = batch_size;
+FashionMNISTLoader::FashionMNISTLoader(int batch_size, bool shuffle_train):
+    m_batch_size(batch_size),
+    m_shuffle_train(shuffle_train)
+{
+
     m_dataset = mnist::read_dataset<std::vector, std::vector, float, uint8_t>("external/fashion_mnist/data/fashion");
 
-    // Create training and test indices
-    training_indices.resize(m_dataset.training_images.size());
-    std::iota(std::begin(training_indices), std::end(training_indices), 0);
+    auto training_size = m_dataset.training_images.size();
+    auto testing_size = m_dataset.test_images.size();
 
-    test_indices.resize(m_dataset.test_images.size());
-    std::iota(std::begin(test_indices), std::end(test_indices), 0);
+    // Create training and test indices
+    m_training_indices.resize(training_size);
+    m_test_indices.resize(testing_size);
 
     // Set Batch Size
-    m_train_num_batches = training_indices.size() / m_batch_size;
-    m_test_num_batches = test_indices.size() / m_batch_size;
+    m_train_num_batches = (training_size - 1) / m_batch_size;
+    m_test_num_batches = (testing_size - 1) / m_batch_size;
 
-    std::cout << "Training Size: " << m_dataset.training_images.size() << std::endl;
-    std::cout << "Test Size: " << m_dataset.test_images.size() << std::endl;
+    std::cout << "Training Batches: " << m_dataset.training_images.size() << std::endl;
+    std::cout << "Test Batches: " << m_dataset.test_images.size() << std::endl;
+};
+
+void FashionMNISTLoader::setTrainIndices() {
+
+    std::iota(std::begin(m_training_indices), std::end(m_training_indices), 0);
 
     // Shuffle training indices
-    if (shuffle_train) {
+    if (m_shuffle_train) {
         auto engine = std::default_random_engine {};
-        std::shuffle(std::begin(training_indices), std::end(training_indices), engine);
+        std::shuffle(std::begin(m_training_indices), std::end(m_training_indices), engine);
     }
+
+}
+
+void FashionMNISTLoader::setTestIndices() {
+
+    std::iota(std::begin(m_test_indices), std::end(m_test_indices), 0);
+
+}
+
+std::vector<Batch> FashionMNISTLoader::loadTrainingBatches() {
+
+    setTrainIndices();
+
+    auto number_of_images = m_batch_size * m_train_num_batches;
+
+    auto batches = loadBatches(m_dataset.training_images, m_dataset.training_labels, m_training_indices, number_of_images);
+
+    return batches;
+
 };
 
-Batch FashionMNISTLoader::nextTrainingBatch() {
+std::vector<Batch> FashionMNISTLoader::loadTestBatches() {
+
+    setTestIndices();
+
+    auto number_of_images = m_batch_size * m_test_num_batches;
+
+    auto batches = loadBatches(m_dataset.test_images, m_dataset.test_labels, m_test_indices, number_of_images);
+
+    return batches;
+
+};
+
+std::vector<Batch> FashionMNISTLoader::loadBatches(std::vector<std::vector<float>> image_set, std::vector<uint8_t> label_set, std::vector<int> indices, int number_of_images) {
+
+    
+    std::vector<std::vector<float>> images;
+    std::vector<int> labels;
+    std::vector<Batch> batches;
+
+    int row_index = 0;
+    
+    for (int image_index = 0; image_index < number_of_images; image_index++) {
+
+        images.push_back(image_set[indices[image_index]]);
+        labels.push_back((int)label_set[indices[image_index]]);
+
+        row_index++;
+
+        if (row_index == m_batch_size) {
+            Batch batch = createBatch(images, labels);
+
+            batches.push_back(batch);
+
+            row_index = 0;
+            images.clear();
+            labels.clear();
+        }
+        
+    }
+
+    return batches;
+
+}
+
+Batch FashionMNISTLoader::createBatch(std::vector<std::vector<float>> images, std::vector<int> labels) {
 
     MatrixXf matrix_batch(m_batch_size, 784);
-    VectorXf row;
-    std::vector<float> image;
-    std::vector<int> labels;
-    Batch next_batch;
-    
-    int idx = 0;
 
-    while (idx < m_batch_size && training_indices.size() > m_batch_size) {
-
-        image = m_dataset.training_images[training_indices.back()];
-        matrix_batch.row(idx) = Eigen::VectorXf::Map(&image[0], image.size());;
-        
-        labels.push_back(m_dataset.training_labels[training_indices.back()]);
-        
-        training_indices.pop_back();
-
-        idx++;
+    for (int row_index = 0; row_index < images.size(); row_index++) {
+        matrix_batch.row(row_index) = VectorXf::Map(&images[row_index][0], images[row_index].size());
     }
 
-    next_batch.images = matrix_batch;
-    next_batch.labels = labels;
+    Batch batch;
 
-    return next_batch;
-};
+    batch.images = matrix_batch;
+    batch.labels = labels;
 
-Batch FashionMNISTLoader::nextTestBatch() {
+    return batch;
 
-    MatrixXf matrix_batch(m_batch_size, 784);
-    VectorXf row;
-    std::vector<float> image;
-    std::vector<int> labels;
-    Batch next_batch;
-    
-    int idx = 0;
-
-    while (idx < m_batch_size && test_indices.size() > m_batch_size) {
-
-        image = m_dataset.training_images[test_indices.back()];
-        matrix_batch.row(idx) = Eigen::VectorXf::Map(&image[0], image.size());;
-        
-        labels.push_back(m_dataset.test_labels[test_indices.back()]);
-        
-        test_indices.pop_back();
-
-        idx++;
-    }
-
-    next_batch.images = matrix_batch;
-    next_batch.labels = labels;
-
-    return next_batch;
-};
+}
 
 int FashionMNISTLoader::num_training_batches() {
     return m_train_num_batches;
--- src/DataLoaders/fashion_mnist_loader.h
+++ src/DataLoaders/fashion_mnist_loader.h
@@ -4,6 +4,7 @@
 #include "base_data_loader.h"
 #include <Eigen/Dense>
 #include <vector>
+#include <queue>
 #include "mnist/mnist_reader.hpp"
 
 using Eigen::MatrixXf;
@@ -14,15 +15,21 @@ private:
     int m_batch_size;
     int m_train_num_batches;
     int m_test_num_batches;
+    bool m_shuffle_train;
 
     mnist::MNIST_dataset<std::vector, std::vector<float>, uint8_t> m_dataset;
-    std::vector<int> training_indices;
-    std::vector<int> test_indices;
+    std::vector<int> m_training_indices;
+    std::vector<int> m_test_indices;
+
+    void setTrainIndices();
+    void setTestIndices();
+    Batch createBatch(std::vector<std::vector<float>> images, std::vector<int> labels);
+    std::vector<Batch> loadBatches(std::vector<std::vector<float>> image_set, std::vector<uint8_t> label_set, std::vector<int> indices, int number_of_images);
 
 public:
     FashionMNISTLoader(int batch_size, bool shuffle_train = true);
-    Batch nextTrainingBatch();
-    Batch nextTestBatch();
+    std::vector<Batch> loadTrainingBatches();
+    std::vector<Batch> loadTestBatches();
     int num_training_batches();
     int num_test_batches();
 };
